b350273dbe2e422af2ecd7d266a371eed8f7929e
Horacio H <hernandezh4@hotmail.com>
2024-08-28 16:16:19 -0700

    Printf format was changed to correctly represent the hexadecimal values.

--- multiplication/multiplication.c
+++ multiplication/multiplication.c
@@ -10,20 +10,12 @@
 
 void measured_function(volatile int *var) {(*var) = 1; }
 
-void mult(long long p1[], long long p2[], long long r[]){
-        /* 
-        unsigned long long tmpSum = p2[i] + carry;
-        r[i] = p1[i] + tmpSum;
-        carry = (p1[i] > r[i]) | (p2[i] > tmpSum);
-        */
-    //unsigned long long carry, sum;
-    //unsigned long long temp;
+void mult(unsigned long long p1[], unsigned long long p2[], unsigned long long r[]){
     for(int i = 0;i<SIZE;i++){
         uint64_t carry = 0;
         for(int j = 0;j<SIZE;j++){
             // (Carry,Sum) <- r[i+j] + a[j] * b[i] + Carry
-            __uint128_t temp = r[i+j] + p1[j] * p2[i] + carry;
-            //sum = temp;
+            __uint128_t temp = r[i+j] + (p1[j] * p2[i] + carry);
             r[i+j] = temp;
             carry = temp >> 64;
         }
@@ -36,13 +28,17 @@ int main(){
     uint64_t start, end;
     int variable = 0;
 
+    //unsigned long long p1[SIZE] = {0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF};
+    //unsigned long long p2[SIZE] = {0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF};
+
     unsigned long long p1[SIZE] = {0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF};
     unsigned long long p2[SIZE] = {0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF};
-    //unsigned long long p1[SIZE] = {1, 2, 3, 4};
-    //unsigned long long p2[SIZE] = {1, 1, 1, 1};
+    //unsigned long long p1[SIZE] = {2, 4, 6, 8};
+    //unsigned long long p2[SIZE] = {1, 2, 3, 4};
 
 
-    long long result[R_SIZE] = {0};
+    unsigned long long result[R_SIZE] = {0};
+
     
     printf("Warming up the cpu.\n");
     for (int i = 0;i<NTEST;i++){
@@ -53,7 +49,7 @@ int main(){
     start = __rdtsc();
     mult(p1, p2, result);
     for(int i = 0;i<R_SIZE;i++){
-        printf("%X\n", result[i]);
+        printf("%016llX\n", result[R_SIZE - 1 - i]);
     }
     end = __rdtsc();
 
