6feb8f86c14ad027402e5e148c9e5a1747aad34d
Horacio H <hernandezh4@hotmail.com>
2024-10-15 11:02:58 -0700

    Montgomery product functions correctly.

--- montgomery/montgomery.c
+++ montgomery/montgomery.c
@@ -48,7 +48,6 @@ void montgomery_pr(unsigned long long p1[], unsigned long long p2[], unsigned lo
     // u = temp / R which is >> 256
     mpz_fdiv_q_2exp(u, temp, SIZE * BIT_LIMIT);
 
-    //SET VALUES OF m and prime to that of values in params.py
     if (mpz_cmp(u, m) >= 0){
         mpz_sub(u, u, m);
     }
@@ -62,20 +61,20 @@ int main(){
     uint64_t start, end;
     int variable = 0;
 
-    unsigned long long p1[SIZE] = {0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF};
-    unsigned long long p2[SIZE] = {0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF};
+    //unsigned long long p1[SIZE] = {0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF};
+    //unsigned long long p2[SIZE] = {0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF};
 
     //unsigned long long p1[SIZE] = {2, 0, 0, 0};
     //unsigned long long p2[SIZE] = {0xFFFFFFFFFFFFFFFF, 0x00000000FFFFFFFF,0x0000000000000000, 0xFFFFFFFF00000001};
     //unsigned long long p2[SIZE] = {0x1900000000000067, 0x175700000000004d, 0xe101d68000000016, 0x2523648240000002};
     
-    //unsigned long long p1[SIZE] = {0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF};
+    unsigned long long p1[SIZE] = {0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF};
     //unsigned long long p2[SIZE] = {0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF};
     //unsigned long long p2[SIZE] = {0, 0, 0, 0};
-    //unsigned long long p1[SIZE] = {2, 0, 0, 0};
+    unsigned long long p2[SIZE] = {2, 0, 0, 0};
     //unsigned long long p2[SIZE] = {5, 0, 0, 0};
 
-    unsigned long long result[SIZE] = {0};
+    unsigned long long result[R_SIZE] = {0};
 
 
     printf("Warming up the cpu.\n");
@@ -86,7 +85,7 @@ int main(){
     printf("Calculating Result...\n");
     start = __rdtsc();
     montgomery_pr(p1, p2, result);
-    for(int i = 0;i<SIZE;i++){
+    for(int i = 0;i<R_SIZE;i++){
         printf("%016llX\n", result[i]);
     }
     end = __rdtsc();
