25e871a75f9b6ed968b6c9cf9ce17bb2a3766df7
Horacio H <hernandezh4@hotmail.com>
2024-10-22 23:34:18 -0700

    Fixed some issues regarding endianess, legendre and the overall tonelli-shanks algorithm

--- sqrt_tonelli_shanks/sqrt.c
+++ sqrt_tonelli_shanks/sqrt.c
@@ -68,74 +68,117 @@ int legendre(mpz_t a, mpz_t p){
     return k;
 }
 
-void sqrt(unsigned long long p1[], unsigned long long p2[], unsigned long long r[]){
-   mpz_t f, p, result, m;
-    mpz_inits(f, p, result, m, NULL);
-
-    mpz_set_str(m, MOD_HEX, 16);
+void t_sqrt(unsigned long long p1[], unsigned long long p2[], unsigned long long r[]){
+    mpz_t f, p, result;
+    mpz_inits(f, p, result, NULL);
 
     mpz_import(f, SIZE, 1, sizeof(unsigned long long), 0, 0, p1);
     mpz_import(p, SIZE, 1, sizeof(unsigned long long), 0, 0, p2);
+    gmp_printf("Value of base f: %Zd\n", f);
+    gmp_printf("Value of modulus p: %Zd\n", p);
 
-    mpz_set(p, m);
-
+    // Check if f is a quadratic residue modulo p
     int legendre_value = legendre(f, p);
     if (legendre_value != 1) {
-        // No square root exists
-        // Set r[0] = -1 to indicate an error
         for (int i = 0; i < R_SIZE; i++) {
             r[i] = 0;
         }
         r[0] = (unsigned long long)-1;
-        mpz_clears(f, p, result, m, NULL);
+        mpz_clears(f, p, result, NULL);
         return;
     }
 
-    mpz_t g, a, temp;
-    mpz_inits(g, a, temp, NULL);
+    if (mpz_cmp_ui(p, 2) == 0) {
+        mpz_set(result, f);
+        size_t count;
+        mpz_export(r, &count, -1, sizeof(unsigned long long), 0, 0, result);
+        mpz_clears(f, p, result, NULL);
+        return;
+    }
 
-    mpz_t expo;
-    mpz_init(expo);
-    mpz_sub_ui(expo, p, 3);
-    mpz_fdiv_q_ui(expo, expo, 4);
+    // Find Q and S such that p - 1 = Q * 2^S, with Q odd
+    mpz_t Q, tmp;
+    unsigned long S = 0;
+    mpz_inits(Q, tmp, NULL);
 
-    //g = f^exponent mod p
-    mpz_powm(g, f, expo, p);
+    mpz_sub_ui(Q, p, 1); // Q = p - 1
 
-    //temp = g^2 mod p
-    mpz_mul(temp, g, g);
-    mpz_mod(temp, temp, p);
+    // Compute S and Q
+    while (mpz_even_p(Q)) {
+        mpz_fdiv_q_2exp(Q, Q, 1); // Q = Q / 2
+        S++;
+    }
 
-    //a = temp * f mod p
-    mpz_mul(a, temp, f);
-    mpz_mod(a, a, p);
+    // Find a quadratic non-residue z modulo p
+    mpz_t z;
+    mpz_init_set_ui(z, 2);
+    while (mpz_legendre(z, p) != -1) {
+        mpz_add_ui(z, z, 1);
+    }
 
-    //g = g * f mod p
-    mpz_mul(g, g, f);
-    mpz_mod(g, g, p);
+    mpz_t c, x, t, b;
+    mpz_inits(c, x, t, b, NULL);
 
-    //if a == p - 1
-    mpz_t p_minus_1;
-    mpz_init(p_minus_1);
-    mpz_sub_ui(p_minus_1, p, 1);
+    // c = z^Q mod p
+    mpz_powm(c, z, Q, p);
 
-    if (mpz_cmp(a, p_minus_1) == 0) {
-        // No square root exists
-        // Set r[0] = -1 to indicate an error
-        for (int i = 0; i < R_SIZE; i++) {
-            r[i] = 0;
+    // x = f^((Q + 1) / 2) mod p
+    mpz_add_ui(tmp, Q, 1);
+    mpz_fdiv_q_2exp(tmp, tmp, 1); // tmp = (Q + 1) / 2
+    mpz_powm(x, f, tmp, p);
+
+    // t = f^Q mod p
+    mpz_powm(t, f, Q, p);
+
+    unsigned long M = S;
+
+    while (mpz_cmp_ui(t, 1) != 0) {
+        mpz_t t2;
+        mpz_init(t2);
+        unsigned long i = 1;
+        mpz_powm_ui(t2, t, 2, p); // t2 = t^2 mod p
+
+        while (i < M && mpz_cmp_ui(t2, 1) != 0) {
+            mpz_powm_ui(t2, t2, 2, p); // t2 = t2^2 mod p
+            i++;
         }
-        r[0] = (unsigned long long)-1;
-        mpz_clears(f, p, result, m, g, a, temp, expo, p_minus_1, NULL);
-        return;
+        mpz_clear(t2);
+
+        if (i == M) {
+            // No square root exists
+            for (int idx = 0; idx < R_SIZE; idx++) {
+                r[idx] = 0;
+            }
+            r[0] = (unsigned long long)-1;
+            mpz_clears(f, p, result, Q, tmp, z, c, x, t, b, NULL);
+            return;
+        }
+
+        // b = c^(2^(M - i - 1)) mod p
+        mpz_powm_ui(b, c, 1UL << (M - i - 1), p);
+
+        // x = x * b mod p
+        mpz_mul(x, x, b);
+        mpz_mod(x, x, p);
+
+        // t = t * b^2 mod p
+        mpz_mul(t, t, b);
+        mpz_mul(t, t, b);
+        mpz_mod(t, t, p);
+
+        // c = b^2 mod p
+        mpz_mul(c, b, b);
+        mpz_mod(c, c, p);
+
+        M = i;
     }
 
-    mpz_set(result, g);
+    mpz_set(result, x);
 
     size_t count;
     mpz_export(r, &count, 1, sizeof(unsigned long long), 0, 0, result);
 
-    mpz_clears(f, p, result, m, g, a, temp, expo, p_minus_1, NULL); 
+    mpz_clears(f, p, result, Q, tmp, z, c, x, t, b, NULL);
 }
 
 int main(){
@@ -145,9 +188,9 @@ int main(){
     //unsigned long long p1[SIZE] = {0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF};
     //unsigned long long p2[SIZE] = {0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF};
 
-    unsigned long long p1[SIZE] = {0, 0, 0, 2};
+    unsigned long long p1[SIZE] = {0, 0, 0, 9};
     //FFFFFFFF00000001000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFF
-    unsigned long long p2[SIZE] = {0xFFFFFFFF00000001, 0x0000000000000000, 0x00000000FFFFFFFF, 0xFFFFFFFFFFFFFFFF};
+    //unsigned long long p2[SIZE] = {0xFFFFFFFF00000001, 0x0000000000000000, 0x00000000FFFFFFFF, 0xFFFFFFFFFFFFFFFF};
     //unsigned long long p2[SIZE] = {0xFFFFFFFFFFFFFFFF, 0x00000000FFFFFFFF, 0x0000000000000000, 0xFFFFFFFF00000001};
     //unsigned long long p2[SIZE] = {0x1900000000000067, 0x175700000000004d, 0xe101d68000000016, 0x2523648240000002};
     
@@ -155,7 +198,7 @@ int main(){
     //unsigned long long p2[SIZE] = {0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF};
     //unsigned long long p2[SIZE] = {0, 0, 0, 0};
     //unsigned long long p2[SIZE] = {2, 0, 0, 0};
-    //unsigned long long p2[SIZE] = {0, 0, 0, 5};
+    unsigned long long p2[SIZE] = {0, 0, 0, 13};
 
     unsigned long long result[R_SIZE] = {0};
 
@@ -167,12 +210,12 @@ int main(){
 
     printf("Calculating Result...\n");
     start = __rdtsc();
-    sqrt(p1, p2, result);
+    t_sqrt(p1, p2, result);
     if (result[0] == (unsigned long long)-1){
         printf("No square root exists.\n");
     } else {
         for(int i = 0;i<R_SIZE;i++){
-        printf("%016lld\n", result[i]);
+        printf("%016llX\n", result[i]);
         }
     }
     end = __rdtsc();
