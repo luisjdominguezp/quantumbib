f578a2495b95d6b42b14cc763d83febda1dfc1a3
Horacio H <hernandezh4@hotmail.com>
2024-10-30 17:45:17 -0700

    Fixing some issues with how bits are read.

--- 0sAnd1s/01.c
+++ 0sAnd1s/01.c
@@ -12,8 +12,10 @@
 void measured_function(volatile int *var) {(*var) = 1; }
 
 int check0s(unsigned long long p[]){
-
+    unsigned long long mask = 0xFFFFFFFFFFFFFFFF;
+    return ((p[4] & mask) != 1) ? 1 : 0;
 }
+
 
 int check1s(unsigned long long p[]){
     
@@ -23,8 +25,11 @@ int main(){
     uint64_t start, end;
     int variable = 0;
 
-    unsigned long long p1[SIZE] = {0x3C134124B7F5C593, 0x58BC963551FADEF0, 0x4153F9BF96E563D2, 0x3BF29687E0FE8C77};
-    unsigned long long p2[SIZE] = {0xAC7242DBB8A9C3C1, 0xE994E8BEE7E830E4, 0xFC2D0E635929E38F, 0x5B5C6B76C6E0A497};
+
+    unsigned long long p1[SIZE] = {0,0,0,0};
+    unsigned long long p2[SIZE] = {0,0,0,1};
+    //unsigned long long p1[SIZE] = {0x3C134124B7F5C593, 0x58BC963551FADEF0, 0x4153F9BF96E563D2, 0x3BF29687E0FE8C77};
+    //unsigned long long p2[SIZE] = {0xAC7242DBB8A9C3C1, 0xE994E8BEE7E830E4, 0xFC2D0E635929E38F, 0x5B5C6B76C6E0A497};
 
     //unsigned long long p1[SIZE] = {0, 0, 0, 4};
     //unsigned long long p2[P_SIZE] = {0xA7081AEA3BDBF56E, 0x0AE5736BE1124F8D, 0xC7CE6E75FAC521DD, 0x9F6A6B593208CDF6, 0x0E83615E354157D9};
@@ -38,10 +43,13 @@ int main(){
 
     printf("Calculating Result...\n");
     start = __rdtsc();
-    check0s(p1);
-    check0s(p2);
+    int resP1 = check0s(p1);
+    int resP2 = check0s(p2);
+    printf("Output of check0s for p1 and p2: %d - %d\n", resP1, resP2);
+    /*
     check1s(p1);
     check1s(p2);
+    */
     end = __rdtsc();
 
     printf("Total = %f CPU cycles\n", (float)(end - start) / NTEST);
