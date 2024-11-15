c776259bb4cc096af00b71546a348c3665155f77
Horacio H <hernandezh4@hotmail.com>
2024-11-15 14:48:17 -0800

    All functions passed the unit tests.

--- benchmarks/benchmark.c
+++ benchmarks/benchmark.c
@@ -1,12 +1,26 @@
 #include <criterion/criterion.h>
+#include <time.h>
 #include "../addition/addition.h"
 #include "../subtraction/subtracion.h"
 #include "../multiplication/multiplication.h"
 #include "../barrett_reduction/reduction.h"
+#include "../exponentiation/expo.h"
+#include "../montgomery/montgomery.h"
+#include "../mont_expo/mont_expo.h"
+#include "../sqrt_tonelli_shanks/sqrt.h"
+#include "../mod_inv/inv.h"
+#include "../random/random.h"
+#include "../check0s/check0s.h"
+#include "../check1s/check1s.h"
+#include "../hash/sha3.h"
 
 #define SIZE 4
+#define P_SIZE 5
 #define R_SIZE 8
+#define BIT_LIMIT 64
 #define bw 64
+#define MOD_HEX "FFFFFFFF00000001000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFF" 
+
 
 Test(addition, add_with_carry_benchmark) {
     unsigned long long p1[SIZE] = {0xFFFFFFFFFFFFFFFF, 0x0, 0x123456789ABCDEF0, 0x9876543210FEDCBA};
@@ -42,3 +56,80 @@ Test(barrett_reduction, barrett_reduction_benchmark) {
     reduc(p1, p2, result, SIZE, R_SIZE, bw);
 }
 
+Test(exponentiation, exponentiation_benchmark) {
+    unsigned long long p1[SIZE] = {0xFFFFFFFFFFFFFFFF, 0x0, 0x123456789ABCDEF0, 0x9876543210FEDCBA};
+    unsigned long long p2[SIZE] = {0xFFFFFFFFFFFFFFFF, 0x00000000FFFFFFFF,0x0000000000000000, 0xFFFFFFFF00000001};
+
+    unsigned long long result[SIZE] = {0};
+    expo(p1, p2, result, SIZE, BIT_LIMIT);
+}
+
+
+Test(montgomery_product, montgomery_product_benchmark) {
+    unsigned long long p1[SIZE] = {0xFFFFFFFFFFFFFFFF, 0x0, 0x123456789ABCDEF0, 0x9876543210FEDCBA};
+    unsigned long long p2[SIZE] = {0xFFFFFFFFFFFFFFFF, 0x00000000FFFFFFFF,0x0000000000000000, 0xFFFFFFFF00000001};
+    mpz_t mod;
+    mpz_init(mod);
+
+    mpz_set_str(mod, MOD_HEX, 16);
+
+    unsigned long long result[SIZE] = {0};
+    montgomery_pr(p1, p2, result, SIZE, BIT_LIMIT, mod);
+}
+
+
+Test(montgomery_expo, montgomery_expo_benchmark) {
+    unsigned long long p1[SIZE] = {0xFFFFFFFFFFFFFFFF, 0x0, 0x123456789ABCDEF0, 0x9876543210FEDCBA};
+    unsigned long long mont_expo[SIZE] = {0xFFFFFFFF00000001, 0x0000000000000000, 0x00000000FFFFFFFF, 0xFFFFFFFFFFFFFFFF};
+    mpz_t mod;
+    mpz_init(mod);
+    mpz_set_str(mod, MOD_HEX, 16);
+
+    unsigned long long result[SIZE] = {0};
+    montgomery_exp(p1, mont_expo, result, SIZE, BIT_LIMIT, mod);
+}
+
+Test(squareroot, squareroot_benchmark) {
+    unsigned long long p1[SIZE] = {0xFFFFFFFFFFFFFFFF, 0x0, 0x123456789ABCDEF0, 0x9876543210FEDCBA};
+    unsigned long long prime2[P_SIZE] = {0xA7081AEA3BDBF56E, 0x0AE5736BE1124F8D, 0xC7CE6E75FAC521DD, 0x9F6A6B593208CDF6, 0x0E83615E354157D9};
+
+    unsigned long long result[SIZE] = {0};
+    t_sqrt(p1, prime2, result, SIZE, R_SIZE);
+}
+
+Test(modular_inverse, modular_inverse_benchmark) {
+    unsigned long long modInv[SIZE] = {0x3C134124B7F5C593, 0x58BC963551FADEF0, 0x4153F9BF96E563D2, 0x3BF29687E0FE8C77};
+    unsigned long long prime2[P_SIZE] = {0xA7081AEA3BDBF56E, 0x0AE5736BE1124F8D, 0xC7CE6E75FAC521DD, 0x9F6A6B593208CDF6, 0x0E83615E354157D9};
+
+    unsigned long long result[SIZE] = {0};
+    inv_mod(modInv, prime2, result, SIZE);
+}
+
+Test(check0s, check0s_benchmark) {
+    unsigned long long p1[SIZE] = {0xFFFFFFFFFFFFFFFF, 0x0, 0x123456789ABCDEF0, 0x9876543210FEDCBA};
+
+    int res = check0s(p1, SIZE);
+}
+
+Test(check1s, check1s_benchmark) {
+    unsigned long long p1[SIZE] = {0xFFFFFFFFFFFFFFFF, 0x0, 0x123456789ABCDEF0, 0x9876543210FEDCBA};
+
+    int res = check1s(p1, SIZE);
+}
+
+Test(random, random_benchmark){
+    unsigned long long p1[SIZE] = {0};
+
+    gmp_randstate_t state;
+    gmp_randinit_default(state);
+
+    unsigned long seed = (unsigned long)time(NULL);
+    gmp_randseed_ui(state, seed);
+    q_random(p1, state, SIZE);
+}
+
+Test(hash, hash_benchmark) {
+    unsigned long long p1[SIZE] = {0xFFFFFFFFFFFFFFFF, 0x0, 0x123456789ABCDEF0, 0x9876543210FEDCBA};
+    unsigned char digest[32];
+    hash_sha3_256((unsigned char *)p1, SIZE, digest);
+}
